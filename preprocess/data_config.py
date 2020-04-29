47f242d0f297be72ce1a48303c19a0c2f539603d
liem <liem@gmail.com>
2020-04-29 17:59:51 +0700

    add config data process

---
+++ preprocess/data_config.py
@@ -0,0 +1,7 @@
+ALLOW_TYPE = ["PNG", "png", "jpg", "JPG"]
+PATH_IMAGE = 'data/images'
+PATH_LABEL = 'data/labels.json'
+IMAGE_SIZE = (1150, 32)
+MAX_LENGHT = 256
+TF_RECORD = "tfrecord_data"
+NUM_NULL_CHAR = 214
